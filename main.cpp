b384f85a403536107a7237e8174757e8e6385575
zacharyj96 <32252338+zacharyj96@users.noreply.github.com>
2018-04-30 01:09:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 #include <gtkmm.h>
 
+// compiler options:
+// `pkg-config gtkmm-3.0 --cflags`
+// `pkg-config glibmm-2.4 --cflags`
+
+// linker settings:
+// `pkg-config gtkmm-3.0 --libs`
+// `pkg-config glibmm-2.4 --libs`
+
 
 int main(int argc, char *argv[]) {
     auto app = Gtk::Application::create(argc,argv,"org.cryptotracker.example");
