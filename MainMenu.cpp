f6e48a5bbcb410a6913ac7d267913de60a9b24fe
zacharyj96 <32252338+zacharyj96@users.noreply.github.com>
2018-05-04 03:29:38 -0400

    Update MainMenu.cpp

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,9 +1,14 @@
 #include "MainMenu.h"
+#include <iostream>
 
 MainMenu::MainMenu()
 {
+    userObj = new CT_User();
     this->resize(400, 200);
     set_border_width(10);
+
+
+
 
     coinNameText.set_placeholder_text("Coin Name");
     coinNameText2.set_placeholder_text("Coin Name");
@@ -40,24 +45,81 @@ MainMenu::MainMenu()
     selector.append_page(secondaryGrid, "Holdings");
     add(selector);
     show_all_children();
+
+}
+
+void MainMenu::createLoginDialog() {
+    loginDialog = new Gtk::Dialog();
+    loginDialog->set_title("Login");
+    loginDialog->set_modal(true);
+    loginDialog->set_transient_for(*this);
+    loginDialog->get_vbox()->add(enterUsername);
+    loginButton.add_label("Login");
+    loginButton.set_size_request(150, 50);
+    loginButton.signal_clicked().connect(sigc::mem_fun(*this, &MainMenu::login));
+    loginDialog->get_vbox()->add(loginButton);
+    loginButton.set_can_default();
+    loginButton.grab_default();
+    loginDialog->show_all_children();
+    loginDialog->set_size_request(200, 100);
+    loginDialog->run();
 }
 
 void MainMenu::coin_button_clicked() {
     std::cout << "Perform coin lookup" << std::endl;
 
-    //CC_API_Calls::get_coin_symbol(coinNameText.get_text());
+    if (userObj->isLoggedIn()) {
+        std::string coinSym = CC_API_Calls::get_coin_symbol(coinNameText.get_text().raw());
+    } else {
+        createLoginDialog();
+    }
+
+
 }
 
 void MainMenu::amount_button_clicked() {
     std::cout << "Perform amount addition" << std::endl;
+
+    if (userObj->isLoggedIn()) {
+        if (coinNameText.get_text().raw().empty()) {
+            // display empty dialog
+        } else {
+            try {
+                int amt = std::stoid(coinAmountText.get_text().raw());
+                userObj.add_coin(coinNameText.get_text().raw());
+                userObj.add_coinID(CC_API_Calls::get_coin_symbol(coinNameText.get_text().raw()), amt);
+            } catch (int e) {
+                // display invalid amount dialog
+            }
+
+        }
+    } else {
+        createLoginDialog();
+    }
 }
 
 void MainMenu::holdings_button_clicked() {
     std::cout << "Perform holdings action" << std::endl;
+
+    if (userObj->isLoggedIn()) {
+
+    } else {
+        createLoginDialog();
+    }
+}
+
+void MainMenu::login() {
+    //cout << enterUsername.get_text().raw() << endl;
+    if (enterUsername.get_text() != "") {
+        userObj = new CT_User(enterUsername.get_text().raw());
+        delete loginDialog;
+    }
 }
 
 
 MainMenu::~MainMenu()
 {
     //dtor
+    //delete userObj;
+
 }
