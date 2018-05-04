2b9b539611fa68f8fe68f6f783a65c79f972b682
zacharyj96 <32252338+zacharyj96@users.noreply.github.com>
2018-05-04 03:30:07 -0400

    Update MainMenu.h

--- MainMenu.h
+++ MainMenu.h
@@ -3,6 +3,7 @@
 
 #include <gtkmm.h>
 #include <iostream>
+#include "CT_User.hpp"
 
 class MainMenu : public Gtk::Window
 {
@@ -12,19 +13,31 @@ class MainMenu : public Gtk::Window
 
     protected:
         //signal handlers
-        void home_button_clicked();
+        void coin_button_clicked();
 
-        void exit_button_clicked();
+        void amount_button_clicked();
+
+        void holdings_button_clicked();
+
+        void login();
+
+        void createLoginDialog();
+
+        CT_User* userObj;
 
         Gtk::Button coinNameButton;
+        Gtk::Button coinNameButton2;
         Gtk::Button coinAmountButton;
+        Gtk::Button loginButton;
+
         Gtk::Grid mainGrid;
         Gtk::Notebook selector;
         Gtk::Entry coinNameText;
         Gtk::Entry coinNameText2;
-        Gtk::Entry coinNameButton2;
         Gtk::Entry coinAmountText;
         Gtk::Grid secondaryGrid;
+        Gtk::Dialog* loginDialog;
+        Gtk::Entry enterUsername;
 
     private:
 };
