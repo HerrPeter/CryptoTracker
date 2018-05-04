5327314c4aa01060619c6b202cc61130c93c6547
zacharyj96 <32252338+zacharyj96@users.noreply.github.com>
2018-05-04 00:48:59 -0400

    Update CT_User.cpp
    Static class references

--- CT_User.cpp
+++ CT_User.cpp
@@ -18,7 +18,7 @@ bool CT_User::add_coin(std::string coin_name) {
   exists = msql.MySQL_fetch()->getInt(1);
 
   if(exists) {
-    std::string coin_symbol = get_coin_symbol(coin_name);
+    std::string coin_symbol = CC_API_Calls::get_coin_symbol(coin_name);
     msql.MySQL_prep_exe("INSERT IGNORE INTO CryptoCoin VALUES(?,?)");
     msql.MySQL_prep()->setString(1, coin_name);
     msql.MySQL_prep()->setString(2, coin_symbol);
@@ -41,8 +41,8 @@ bool CT_User::login() {
   if(exists) {
     msql.MySQL_prep_exe("INSERT IGNORE INTO UserData VALUES(?,?,?)");
     msql.MySQL_prep()->setString(1, u_name);
-    msql.MySQL_prep()->setInt(2, get_timestamp());
-    msql.MySQL_prep()->setInt(3, get_timestamp());
+    msql.MySQL_prep()->setInt(2, CC_API_Calls::get_timestamp());
+    msql.MySQL_prep()->setInt(3, CC_API_Calls::get_timestamp());
     msql.MySQL_prep()->executeUpdate();
   }
 
@@ -92,10 +92,10 @@ bool CT_User::add_coinID(std::string symbol, int amount) {
   msql.MySQL_prep_exe("INSERT INTO UserCoinID (coin_symbol, u_name, add_date, amount, last_price, add_price) VALUES(?,?,?,?,?,?)");
   msql.MySQL_prep()->setString(1, symbol);
   msql.MySQL_prep()->setString(2, u_name);
-  msql.MySQL_prep()->setInt(3, get_timestamp());
+  msql.MySQL_prep()->setInt(3, CC_API_Calls::get_timestamp());
   msql.MySQL_prep()->setInt(4, amount);
-  msql.MySQL_prep()->setDouble(5, std::stod(get_price(symbol)));
-  msql.MySQL_prep()->setDouble(6, std::stod(get_price(symbol)));
+  msql.MySQL_prep()->setDouble(5, std::stod(CC_API_Calls::get_price(symbol)));
+  msql.MySQL_prep()->setDouble(6, std::stod(CC_API_Calls::get_price(symbol)));
   msql.MySQL_prep()->executeUpdate();
   }
   return exists;
@@ -105,7 +105,7 @@ bool CT_User::add_coinID(std::string symbol, int amount) {
 //if an empty string is passed does not log a new user in
 void CT_User::log_out(std::string user) {
   msql.MySQL_prep_exe("UPDATE UserData SET last_date=? WHERE username=?");
-  msql.MySQL_prep()->setInt(1, get_timestamp());
+  msql.MySQL_prep()->setInt(1, CC_API_Calls::get_timestamp());
   msql.MySQL_prep()->setString(2, u_name);
   msql.MySQL_prep()->executeUpdate();
   user_coins();
@@ -214,7 +214,7 @@ int CT_User::get_amount(int coin_id) {
 //returns the current price of 1 coin in USD
 double CT_User::curr_price(int coin_id) {
   long int ts = 0;
-  return std::stod(get_price(coinid_symbol(coin_id),  ts));
+  return std::stod(CC_API_Calls::get_price(coinid_symbol(coin_id),  ts));
 }
 
 //returns the current price of 1 coin from the last day you logged out in USD
@@ -241,7 +241,7 @@ double CT_User::start_price(int coin_id) {
 
 //returns the current worth of 1 coin in terms of other currencies and timestamps
 double CT_User::price_lookup(int coin_id, std::string curr_sym, long int ts) {
-  return std::stod(get_price(coinid_symbol(coin_id), curr_sym, ts));
+  return std::stod(CC_API_Calls::get_price(coinid_symbol(coin_id), curr_sym, ts));
 }
 
 //returns the current worth of the amount of cryptocoins in a coinid in USD (amount * price of 1 coin)
